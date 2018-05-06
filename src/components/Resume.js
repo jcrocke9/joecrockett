15b2616dd347d8ab67ad19a9a08c19d0bfd9ec4b
Joe Crockett <jcrocket@gmail.com>
2018-05-06 19:14:58 -0400

    resume work

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import '../css/resume.min.css';
+import '../vendor/devicons/css/devicons.min.css';
+import '../vendor/simple-line-icons/css/simle-line-icons.css';
 
 export class Resume extends Component {
-
-    linkedIn() { window.location.href = "https://www.linkedin.com/in/josephpcrockett/"; }
-    github() { window.location.href = "https://github.com/jcrocke9/"; }
-    powershellGallery() { window.location.href = "https://www.powershellgallery.com/profiles/jcrocke9/"; }
     render() {
         return (
             <div>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+                    <a className="navbar-brand js-scroll-trigger" href="#page-top">
+                        <span className="d-block d-lg-none">Start Bootstrap</span>
+                        <span className="d-none d-lg-block">
+                            <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile.jpg" alt=""></img>
+                        </span>
+                    </a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#about">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#experience">Experience</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#education">Education</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#skills">Skills</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link js-scroll-trigger" href="#interests">Interests</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
                 <div className="bs-docs-header" id="content" tabIndex="-1">
                     <div className="container">
                         <h1>Resume</h1>
